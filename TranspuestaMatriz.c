ea87bd770301a3ca8219fea6f263f8c45bf74b1b
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-23 21:29:33 -0500

    Agregamos las variables y un funcion para que lea lo que el usuario ingrese oara usar en el tama;o de la matriz al igual de inicializar las matrices y variables

--- TranspuestaMatriz.c
+++ TranspuestaMatriz.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// Kevin Calle Maquis Calle
+int main()
+{
+    // Agregamos la funcion para que no nos de error al usar una funcion que nos brinde valores aleatorios
+    srand(time(NULL));
+    int n, m;
+    // Inicializamos las variables y ponemos que el usuario meta los valores del tama;o de la matriz y leemos eso
+    printf("Ingeres el numero de filas:\n");
+    scanf("%d", &n);
+    printf("Ingeres el numero de columnas:\n");
+    scanf("%d", &m);
+    // Iniciamos las matrices con los valores que el ususario ingreso
+    int Matrix[n][m];
+    int MatrixA[m][n];
+
+    
+}
