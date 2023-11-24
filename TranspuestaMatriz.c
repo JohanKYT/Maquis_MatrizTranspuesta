9d929909cc1be99ac71f45d5f93d75801346c357
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-23 21:38:12 -0500

    Agregamos los ultimos comentarios para entender de mejor manera el programa y probamos que todo este correcto

--- TranspuestaMatriz.c
+++ TranspuestaMatriz.c
@@ -16,30 +16,31 @@ int main()
     int Matrix[n][m];
     int MatrixA[m][n];
 
-    printf("\nLA MATRIZ ORIGINAL ES:\n");
+    printf("\nLA MATRIZ ORIGINAL ES:\n"); // Agregamos un print que nos diga donde empieza la matriz original
 
     for (int i = 0; i < n; i++)
     {
-
         printf("\n");
         for (int j = 0; j < m; j++)
         {
+            //Usamos la funcion donde nos seleccione numeros aleatorios de o al 100
             Matrix[i][j] = rand() % 100 + 1;
             printf("%d\t|\t", Matrix[i][j]);
         }
     }
+    
     printf("\n");
-    printf("\n LA MATRIZ TRANSPUESTA ES:\n");
+    printf("\n LA MATRIZ TRANSPUESTA ES:\n"); // Agregamos un print que nos diga donde empieza la matriz transpuesta
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)//Cambiamos 'm' por 'n' para poder realizar una inversa que no sea cuadratica asi el usuario puede ingresar cualquier valor 
     {
-
         printf("\n");
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)//Cambiamos 'n' por 'm' para poder realizar una inversa que no sea cuadratica asi el usuario puede ingresar cualquier valor 
         {
+            //Usamos un igual para revertir la matriz y sacar la transpuesta de la original y nos imprima eso con el segundo for
             MatrixA[i][j] = Matrix[j][i];
-
             printf("%d\t|\t", MatrixA[i][j]);
         }
     }
+return 0;
 }
